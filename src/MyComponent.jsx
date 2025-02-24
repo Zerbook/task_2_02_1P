cfde617057964430e45a05a013fff2a4376b18c1
Zerbook <doktorwest1@rambler.ru>
2025-02-24 16:51:53 +0300

    24.02.2025 work

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -1,14 +1,32 @@
 import { useState } from "react"
+import styles from './MyComponent.module.css'
 
 const getTimeFromDate = (date) => date.toISOString().substring(11, 19)
 
-const products = [
-	{ id: 'qwe', name: 'Чайник' },
-	{ id: 'rty', name: 'Утюг' }
-]
+// const products = [
+// 	{ id: 'qwe', name: 'Чайник' },
+// 	{ id: 'rty', name: 'Утюг' }
+// ]
 
 export const MyComponent = () => {
-	// const [currentDate, setCurrentDate] = useState(Math.random())
+	const [value, setValue] = useState(0)
+	const [showRedText, setshowRedText] = useState(false)
+	const onClick = () => {
+		setshowRedText(!showRedText)
+	}
+
+	const text = <div className={showRedText ? styles.red : styles.white}>Текст</div>
+
+	return (
+		<>
+			{text}
+			<button onClick={onClick}>Изменить цвет текста</button>
+		</>
+	)
+}
+
+
+// const [currentDate, setCurrentDate] = useState(Math.random())
 	// setTimeout(() => {
 	// 	setCurrentDate(Math.random())
 	// }, 1000)
@@ -21,10 +39,9 @@ export const MyComponent = () => {
 	// 	<div>{obj.a}</div>
 	// )
 
-	return <ul>
-		{products.map(({ id, name }) =>
-			<li key={id}>{name}</li>
-		)}
-	</ul>
+	// return <ul>
+	// 	{products.map(({ id, name }) =>
+	// 		<li key={id}>{name}</li>
+	// 	)}
+	// </ul>
 
-}
